d2fcd2c1ff2a912f7bb4ff13fb5971f9ff4ecfca
kepoo <dhikagamergz@gmail.com>
2023-08-20 22:53:02 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// clear form
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByName("form")) {
+        form.reset();
+    }
+};
+
+// toogle 
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
